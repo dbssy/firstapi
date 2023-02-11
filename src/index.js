46b29b8e07984da7a89f22e8e3247621847a30a0
João Debussy <social.debussy@gmail.com>
2023-02-11 13:13:38 -0300

    fix: edit request method on index; feat: create User controller

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
 
   const route = routes.find((routeObj) => (
-    routeObj.endpoint === request.endpoint && routeObj.method === request.method
+    routeObj.endpoint === request.url && routeObj.method === request.method
   ));
 
   if (route) {
-    routes.handler(request, response);
+    route.handler(request, response);
   } else {
     response.writeHead(404, { 'Content-Type': 'text/html' });
     response.end(`Cannot ${request.method} ${request.url}`);
